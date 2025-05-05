6ced1bddbd495c11dddf80830fb7c595a824eae1
er <er@er.com>
2025-05-05 14:49:15 -0400

    add srv add

--- prch_setup.sh
+++ prch_setup.sh
@@ -1,4 +1,71 @@
-read -p "Enter pricechecker IP: " pricecheckerip
+#!/bin/bash
+echo "Please select ACR server from the list:"
+echo "1) 65st"
+echo "2) E16st"
+echo "3) Philly 1"
+echo "4) 18th ave"
+echo "5) Staten Iland"
+echo "6) Queens"
+echo "7) Brighton"
+echo "8) Philly2"
+echo "9) Manalapan"
+echo "10) Paramus"
+echo "11) Ocean"
+echo "12) Neptune"
+echo "13) Florida(Oakwood)"
+echo "14) exit"
+read -p "Enter your choice [1-14]: " choice
+case $choice in
+    1)
+        pricecheckersrv="10.0.1.5"
+        ;;
+    2)
+        pricecheckersrv="10.0.2.5"
+        ;;
+    3)
+        pricecheckersrv="10.0.4.5"
+        ;;
+    4)
+        pricecheckersrv="10.0.5.5"
+        ;;
+    5)
+        pricecheckersrv="10.0.6.5"
+        ;;
+    6)
+        pricecheckersrv="10.0.7.5"
+        ;;
+    7)
+        pricecheckersrv="10.0.8.5"
+        ;;
+    8)
+        pricecheckersrv="10.0.9.5"
+        ;;
+    9)
+        pricecheckersrv="10.0.10.5"
+        ;;
+    10)
+        pricecheckersrv="10.0.11.5"
+        ;;
+    11)
+        pricecheckersrv="10.0.12.5"
+        ;;
+    12)
+        pricecheckersrv="10.0.14.5"
+        ;;
+    13)
+        pricecheckersrv="10.0.15.5"
+        ;;
+    14)
+        echo "Exiting..."
+        exit 0
+        ;;
+    *)
+        echo "Invalid selection."
+        exit 1
+        ;;
+esac
+echo "You selected: $pricecheckersrv"
+
 
 # Install X.org, OpenBox, Firefox
 sudo apt install --no-install-recommends -y xorg openbox firefox
@@ -11,7 +78,7 @@ xset -s noblank
 off openbox-session &
 while true;
 do
-  /usr/bin/firefox -kiosk -private-window -width=1280 -height=800 http://${pricecheckerip}:4322/access/PriceChecker.html
+  /usr/bin/firefox -kiosk -private-window -width=1280 -height=800 http://${pricecheckersrv}:4322/access/PriceChecker.html
 done" | sudo tee /opt/kiosk/kiosk.sh
 
 # Write service file to /etc/systemd/system/kiosk.service
